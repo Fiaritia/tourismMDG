e906f1d83085ef6d7f403dca9510e8f1d366a1d9
Fiaritia <rahamisonf@gmail.com>
2023-08-06 19:03:33 +0300

    Splash screen init.

--- app/src/main/java/com/example/tourismmdg/MainActivity.kt
+++ app/src/main/java/com/example/tourismmdg/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.tourismmdg
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
+    private val splashTime = 3000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
+
+        // Décaler le passage à l'écran d'accueil après les 3s
+        Handler().postDelayed({
+            val intent = Intent(this@MainActivity, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, splashTime.toLong())
     }
 }
