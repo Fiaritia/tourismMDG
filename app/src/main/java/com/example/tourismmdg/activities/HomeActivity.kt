9c6f22ec87032ca4e4689cf93c4a88c2ee810dd1
Fiaritia <rahamisonf@gmail.com>
2023-08-08 01:46:20 +0300

    mise en place du contenu vidéo

--- app/src/main/java/com/example/tourismmdg/activities/HomeActivity.kt
+++ app/src/main/java/com/example/tourismmdg/activities/HomeActivity.kt
@@ -1,6 +1,10 @@
 package com.example.tourismmdg.activities
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 import com.example.tourismmdg.R
 import com.example.tourismmdg.databinding.ActivityHomeBinding
@@ -16,6 +20,11 @@ class HomeActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_home)
 
+        val webview = findViewById<WebView>(R.id.webview)
+        webview.webViewClient = WebViewClient()
+        webview.loadUrl("https://www.youtube.com/watch?v=KnCFzro1jU8")
+        webview.settings.javaScriptEnabled = true
+        webview.settings.setSupportZoom(true)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.headerFragment, HeaderFragment())
