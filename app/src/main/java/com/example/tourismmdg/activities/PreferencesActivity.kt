321b70ec8c8c89f60de268ec554612ff0d379854
Fiaritia <rahamisonf@gmail.com>
2023-08-08 03:19:28 +0300

    Page des préférences

---
+++ app/src/main/java/com/example/tourismmdg/activities/PreferencesActivity.kt
@@ -0,0 +1,52 @@
+package com.example.tourismmdg.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatDelegate
+import com.example.tourismmdg.R
+
+class PreferencesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_preferences)
+    }
+
+    private lateinit var modeToggleImageView: ImageView
+    private var isNightMode = false
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_item, container, false)
+
+        modeToggleImageView = view.findViewById(R.id.modeToggleImageView)
+        modeToggleImageView.setOnClickListener {
+            toggleDayNightMode()
+        }
+
+        return view
+    }
+
+    private fun toggleDayNightMode() {
+        // Toggle the night mode flag
+        isNightMode = !isNightMode
+
+        // Set the appropriate icon based on the mode
+        if (isNightMode) {
+            modeToggleImageView.setImageResource(R.drawable.ic_dark_mode)
+            // Set your night mode theme here
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        } else {
+            modeToggleImageView.setImageResource(R.drawable.ic_day_mode)
+            // Set your day mode theme here
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
+    }
+
+}
